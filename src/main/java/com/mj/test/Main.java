a2d89d72f044d337dd9f1fe392b1b26fd880bdb8
liuqian <liuq@linkstec.com>
2020-06-16 09:04:29 +0800

    删除ppt

--- src/main/java/com/mj/test/Main.java
+++ src/main/java/com/mj/test/Main.java
@@ -2,6 +2,9 @@ package com.mj.test;
 
 public class Main {
     public static void main(String[] args) {
+        String s1 = "abbacde";
+        String s2 = "bacd";
+        System.out.println(s1.indexOf(s2));
         System.out.println("Hello World");
     }
 }
